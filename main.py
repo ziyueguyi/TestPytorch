87623f0a12c39b0e7631d79f02d03b8ff8d2913e
会做饭的菜鸡 <2275914231@qq.com>
2022-09-15 07:52:46 +0800

    第一次提交

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# !-*-coding: UTF-8 -*-
+"""
+文件名称：main.py
+项目名称：Test——Pytorch
+项目作者：会做饭的菜鸡 
+作者邮箱：2275914231@qq.com 
+创建时间：2022/9/14 21:43
+"""
+
+if __name__ == '__main__':
+    pass
